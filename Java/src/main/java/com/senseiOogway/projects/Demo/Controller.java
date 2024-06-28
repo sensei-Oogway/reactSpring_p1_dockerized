1bb87cebfe5d7348de828369b4ef7b9c4a286d4a
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2024-06-28 12:14:41 +1000

    React Changes

--- Java/src/main/java/com/senseiOogway/projects/Demo/Controller.java
+++ Java/src/main/java/com/senseiOogway/projects/Demo/Controller.java
@@ -3,11 +3,13 @@ package com.senseiOogway.projects.Demo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class Controller {
     @GetMapping("/hello")
-    public ResponseEntity hello(){
-        return ResponseEntity.ok("Hello@ Don");
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("DON!");
     }
 }
