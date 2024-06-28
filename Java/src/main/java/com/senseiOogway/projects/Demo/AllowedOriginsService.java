00f4e1ced6b0d2feac40e0c690733fcb0d2654c3
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2024-06-28 19:01:29 +1000

    Docker java changes

---
+++ Java/src/main/java/com/senseiOogway/projects/Demo/AllowedOriginsService.java
@@ -0,0 +1,16 @@
+package com.senseiOogway.projects.Demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AllowedOriginsService {
+    @Autowired
+    private Environment env;
+
+    public String getAllowedOrigins() {
+        return env.getProperty("allowed.origins","http://localhost:3000");
+    }
+}
--- Java/src/main/java/com/senseiOogway/projects/Demo/Controller.java
+++ Java/src/main/java/com/senseiOogway/projects/Demo/Controller.java
@@ -1,5 +1,7 @@
 package com.senseiOogway.projects.Demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    private AllowedOriginsService allowedOriginsService;
+
     @GetMapping("/hello")
-    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<String> hello(){
-        return ResponseEntity.ok("DON!");
+        return ResponseEntity.ok().header("Access-Control-Allow-Origin", allowedOriginsService.getAllowedOrigins()).body("DON!");
     }
 }
