0ca85a10daa011e4ee1f1a3d235640450da2279c
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2024-07-03 13:53:31 +1000

    changes

--- Java/src/main/java/com/senseiOogway/projects/Demo/AllowedOriginsService.java
+++ Java/src/main/java/com/senseiOogway/projects/Demo/AllowedOriginsService.java
@@ -1,7 +1,6 @@
 package com.senseiOogway.projects.Demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
--- Java/src/main/java/com/senseiOogway/projects/Demo/Controller.java
+++ Java/src/main/java/com/senseiOogway/projects/Demo/Controller.java
@@ -1,20 +1,26 @@
 package com.senseiOogway.projects.Demo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class Controller {
-
     @Autowired
     private AllowedOriginsService allowedOriginsService;
 
     @GetMapping("/hello")
-    public ResponseEntity<String> hello(){
+    public ResponseEntity<String> hello(HttpServletRequest request){
+
+        String domain = request.getServerName();
+        int port = request.getServerPort();
+        String protocol = request.getScheme();
+
+        String domainInfo = protocol + "://" + domain + ":" + port;
+
+        System.out.println(domainInfo);
         return ResponseEntity.ok().header("Access-Control-Allow-Origin", allowedOriginsService.getAllowedOrigins()).body("DON!");
     }
 }
