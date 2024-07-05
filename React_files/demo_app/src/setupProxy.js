7f7b8ba29b2eb46ede2efa1c8ddcb5a92289d5e8
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2024-07-05 13:33:18 +1000

    middleware changes react

--- React_files/demo_app/src/setupProxy.js
+++ React_files/demo_app/src/setupProxy.js
@@ -1,15 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+
 module.exports = function (app) {
   app.use(
-    "/api",
-
     createProxyMiddleware({
       target: process.env.REACT_APP_PROXY_ADDR,
       changeOrigin: true,
-      pathRewrite:{
-        '^/api': ""
-      }
+      pathFilter: '/api',
+      pathRewrite: {'^/api(.*)' : '/api/hello'}
     })
   );
 };
